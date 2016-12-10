ba69972fe74e1341809c79351cadb69449f64027
ramesh7128 <ramesh7128@gmail.com>
2016-12-10 19:12:12 +0530

    added response with a user name

--- movietimeBot/movietime/views.py
+++ movietimeBot/movietime/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.views import generic
 from django.utils.decorators import method_decorator
@@ -8,7 +9,10 @@ import requests
 
 # Create your views here.
 
+
 class MovieTimeBot(generic.View):
+
+	access_token = settings.ACCESS_TOKEN
 
 	@method_decorator(csrf_exempt)
 	def dispatch(self, request, *args, **kwargs):
@@ -32,9 +36,14 @@ class MovieTimeBot(generic.View):
 					print message['sender']['id']
 					fb_id = message['sender']['id']
 					user_details_url = "https://graph.facebook.com/v2.6/%s"%fb_id
-					user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':'<page-access-token>'}
+					user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':'%s' % self.access_token}
 					user_details = requests.get(user_details_url, user_details_params).json()
 					print user_details
-			return HttpResponse()
+					first_name = user_details['first_name']
+					check_message = "Yo, %s!!" % first_name
+					post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % self.access_token 
+					response_msg = json.dumps({"recipient":{"id":fb_id}, "message":{"text": check_message}})
+					status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+					return HttpResponse()
 
 
